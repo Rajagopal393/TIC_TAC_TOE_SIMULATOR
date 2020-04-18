cb4dd739449b649485192619550c94c464941bc6
Raj Gopal <harirajagopal393@gmail.com>
2020-04-18 12:29:25 +0530

    Added Toss simulator feature to decide the first person to make a move

--- ticTacToeSimulator.sh
+++ ticTacToeSimulator.sh
@@ -4,9 +4,20 @@ assignSign=$((RANDOM%2))
 isXsign=1
 isOsign=0
 
-if [ $assignSign -eq isXsign ]
+if [ $assignSign -eq $isXsign ]
 	then
 		echo "Your sign is 'X'"
 else
 		echo "Your sign is 'O'"
+fi
+
+tossForFirstChance=$((RANDOM%2))
+isYou=1
+isNotYou=0
+
+if [ $tossForFirstChance -eq $isYou ]
+	then
+		echo "You are first to start the game...choose one box"
+	else
+		echo "Your opponents turn...wait for him to make move"
 fi
